0b36dc9047ce87d066873ed085f691ee8e32ea89
Osahon Oboite <osahonoboite@gmail.com>
2026-04-15 18:14:58 +0100

    chore: configure rate limit

--- src/infra/config/rate-limiter.config.ts
+++ src/infra/config/rate-limiter.config.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
 import rateLimit, { ipKeyGenerator } from 'express-rate-limit';
 
+/**
+ * 700 requests per 15 minutes
+ */
 export const RATE_LIMITER_WINDOW_MS = 15 * 60 * 1000;
-export const RATE_LIMITER_MAX = 100;
+export const RATE_LIMITER_MAX = 700;
 
 export const RATE_LIMITER_MESSAGE =
   'Too many requests from this IP, please try again later.';
