e4386105053f93d47efe879f190aa8beab9949e3
Osahon Oboite <osahonoboite@gmail.com>
2026-06-10 12:42:03 +0100

    chore: cleanup

--- src/app/_bootstrap/index.ts
+++ src/app/_bootstrap/index.ts
@@ -1,5 +1,4 @@
 import eventBus from '../../infra/messaging/bus/event-bus';
-import registerExchangeRateConsumer from '../../infra/messaging/external/exchange-rate.consumer';
 import registerWorkers from '../../infra/messaging/workers';
 import observability from '../../infra/observability';
 import repos from '../../infra/persistence/repos';
@@ -7,6 +6,7 @@ import setupServer from '../../infra/server';
 import services from '../../infra/services';
 import { bootstrapAccountingContext } from './accounting-context.bootstrap';
 import eventsRegistry from './events.bootstrap';
+import registerRabbitMQConsumers from './rabbit-mq-consumers.bootstrap';
 import bootstrapCurrencies from './setup-currencies.bootstrap';
 
 async function bootstraper() {
@@ -22,7 +22,7 @@ async function bootstraper() {
 
     registerWorkers(observability.reporter);
     eventsRegistry(eventBus);
-    await registerExchangeRateConsumer(observability.reporter);
+    await registerRabbitMQConsumers();
   } catch (error) {
     observability.logger.error(error, 'Failed to bootstrap');
     observability.reporter.report(error);
---
+++ src/app/_bootstrap/rabbit-mq-consumers.bootstrap.ts
@@ -0,0 +1,8 @@
+import registerExchangeRateConsumer from '../../infra/messaging/external/exchange-rate.consumer';
+import observability from '../../infra/observability';
+
+export default async function registerRabbitMQConsumers() {
+  const consumers = [registerExchangeRateConsumer(observability.reporter)];
+
+  await Promise.all(consumers).catch(observability.reporter.report);
+}
